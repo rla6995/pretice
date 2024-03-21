090da6d5f2621cc455a81d79a9b698e8d75f6057
rla6995 <164276503+rla6995@users.noreply.github.com>
2024-03-21 09:48:50 +0900

    Update Forssil_Hardwood_reference.py

--- Forssil_Hardwood_reference.py
+++ Forssil_Hardwood_reference.py
@@ -9,7 +9,7 @@ import re
 import numpy as np
 Forssil_Hardwood=[]
 sub_wood=[]
-alpabet=[2,3,4,5,6,7,8,9,10,12,13,14,15,16,18,19,20,21,22]
+alpabet=[2,3,4,5,6,7,8,9,10,12,13,14,15,16,18,19,20,21,22,99]
 wd = webdriver.Chrome()
 wd.get("https://insidewood.lib.ncsu.edu/taxtree/typeofwood/FH/letter/a/taxtype/family?1")
 for var in alpabet:
@@ -97,8 +97,9 @@ for var in alpabet:
         else:
             pass
         wd.back()
-    wd.find_element(By.XPATH, (f"/html/body/table[3]/tbody/tr/td[4]/table[1]/tbody/tr/td[{var}]/a")).click()
-    time.sleep(5)
+    if var != 99:
+        wd.find_element(By.XPATH, (f"/html/body/table[3]/tbody/tr/td[4]/table[1]/tbody/tr/td[{var}]/a")).click()
+        time.sleep(5)
 a=[]
 b=[]        
 c=[]
